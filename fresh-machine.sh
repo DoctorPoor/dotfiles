262676b3896f6b712f947cd735434f832f6897a3
Perry Tiu <DoctorPoor@users.noreply.github.com>
2019-11-30 02:16:19 +0000

    Added zsh and some oher brew install

--- fresh-machine.sh
+++ fresh-machine.sh
@@ -1,2 +1,11 @@
 # install brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+#install zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+brew install git
+brew install node
+brew install n
+brew install htop
+
