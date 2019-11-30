7b3095578fc35cb460fed654297fae3e2efd9c63
Perry Tiu <DoctorPoor@users.noreply.github.com>
2019-11-30 02:22:42 +0000

    Added coupla brew cask installs

--- fresh-machine.sh
+++ fresh-machine.sh
@@ -1,11 +1,23 @@
+# mac settings like show hidden files, mouse speed, trackpad speed, keyboard key repeat, etc
+
 # install brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 #install zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+# git configs
+
 brew install git
+brew install htop
 brew install node
 brew install n
-brew install htop
+
+brew cask install iterm2
+brew cask install google-chrome
+brew cask install spotify
+brew cask install whatsapp
+brew cask install clipy
+brew cask install sublime-text
+brew cask install visual-studio-code
 
