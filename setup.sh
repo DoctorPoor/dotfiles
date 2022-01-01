68516aaf40fc6dd3dbdb020c4cbce078dd71f73e
perry <8948878+DoctorPoor@users.noreply.github.com>
2021-12-31 18:34:00 -0800

    Added more macOS defaults

--- setup.sh
+++ setup.sh
@@ -1,23 +1,28 @@
-#
+################################
 # MacOS Defaults
-#
+################################
 
-defaults write com.apple.Finder AppleShowAllFiles true    # show hidden files
-defaults write -g com.apple.mouse.scaling 12    # bump mouse speed
-defaults write -g com.apple.trackpad.scaling 12    # bump trackpad speed
-defaults write -g ApplePressAndHoldEnabled -bool false    # disable keyboard press and hold
-defaults write -g InitialKeyRepeat 15    # lower keyboard initial key repeat
-defaults write -g KeyRepeat 2    # bump key repeat speed
+defaults write com.apple.Finder AppleShowAllFiles true                                                     # show hidden files
+defaults write -g com.apple.mouse.scaling 12                                                               # bump mouse speed
+defaults write -g com.apple.trackpad.scaling 12                                                            # bump trackpad speed
+defaults write -g ApplePressAndHoldEnabled -bool false                                                     # disable keyboard press and hold
+defaults write -g InitialKeyRepeat 15                                                                      # lower keyboard initial key repeat
+defaults write -g KeyRepeat 2                                                                              # bump key repeat speed
+defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -int 0                                  # prevent period after double-space
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false                              # disable smart quotes
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false                               # disable smart dashes
+defaults write com.apple.TextEdit SmartQuotes -bool false                                                  # disable smart quotes for TextEdit
+defaults write com.apple.TextEdit SmartDashes -bool false                                                  # disable smart dashes for TextEdit
 
-#
+################################
 # Tooling
-#
+################################
 
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"    # install brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"         # install brew
 echo 'eval $(/opt/homebrew/bin/brew shellenv)' >> /Users/$USER/.zprofile
 eval $(/opt/homebrew/bin/brew shellenv)
 
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"    # install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"                       # install oh-my-zsh
 
 brew tap doctorpoor/pwdrng
 brew install \
@@ -42,8 +47,8 @@ brew install --cask \
   whatsapp \
   visual-studio-code \
 
-cp ./zsh/perry.zsh-theme ~/.oh-my-zsh/themes/    # add zsh theme
-cp ./zsh/.zshrc ~    # update .zshrc
+cp ./zsh/perry.zsh-theme ~/.oh-my-zsh/themes/                                                              # add zsh theme
+cp ./zsh/.zshrc ~                                                                                          # update .zshrc
 
-mkdir -p ~/.vim/colors; cp ./vim/.vim/colors/monokai.vim ~/.vim/colors/    # add vim theme
-cp ./vim/.vimrc ~    # update .vimrc
+mkdir -p ~/.vim/colors; cp ./vim/.vim/colors/monokai.vim ~/.vim/colors/                                    # add vim theme
+cp ./vim/.vimrc ~                                                                                          # update .vimrc
